d6929e2e2d6703d3e5b11417192208daa64e0458
Julian Lechner <jll38@njit.edu>
2023-09-23 19:13:40 -0400

    pwa integration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+const withPWA = require('next-pwa');
 
-module.exports = nextConfig
+
+module.exports = withPWA({
+    pwa: {
+      dest: 'public'
+    }
+  });
