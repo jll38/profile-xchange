443c5d2fd38683a2ef65740401a982f8c1c658ce
Julian Lechner <jll38@njit.edu>
2023-09-23 20:16:30 -0400

    >:(

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { xChangeLogo } from "./../../public/images/xChange_logo_transparent.png";
+
 import { UserAuthForm } from "./../components/auth/loginForm";
 
 export default function Home() {
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white z-20">
       <div className="min-w-[20rem] flex flex-col justify-center items-center ">
         <div id="logo-header" className="text-center mb-[2rem]">
-          <div className="logo flex justify-center"><Image src={xChangeLogo} alt="" width={100} height={100} /></div>
+          <div className="logo flex justify-center"><Image src="/images/xChange_logo_transparent.png" alt="" width={100} height={100} /></div>
           <div className="text-[4em] font-extrabold">xChange</div>
           <div className="-mt-[.75rem] text-[.8rem] text-slate-300">Unlock Connections, XChange Value</div>
         </div>
