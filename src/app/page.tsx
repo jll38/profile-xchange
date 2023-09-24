3b95d8c95c634b8545cf89044c1169b3d271bd82
Vladimir <vgutierrez.four@gmail.com>
2023-09-24 02:34:36 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white z-20">
       <div className="min-w-[20rem] flex flex-col justify-center items-center ">
         <div id="logo-header" className="text-center mb-[2rem]">
-          <div className="logo flex justify-center"><Image src="/images/xChange_logo_transparent.png" alt="" width={100} height="auto" /></div>
+          <div className="logo flex justify-center"><Image src="/images/xChange_logo_transparent.png" alt="" width={100} /></div>
           <div className="text-[4em] font-extrabold">xChange</div>
           <div className="-mt-[.75rem] text-[.8rem] text-slate-300">Unlock Connections, XChange Value</div>
         </div>
